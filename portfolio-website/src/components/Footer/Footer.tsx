6451c16c84484a5b9fc3fdb7e2edffe2dc29bf63
Jan Kierejsza <jan.kierejsza@gmail.com>
2024-01-10 17:22:54 +0100

    small-screen-adjustments

--- portfolio-website/src/components/Footer/Footer.tsx
+++ portfolio-website/src/components/Footer/Footer.tsx
@@ -3,8 +3,10 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="box"></div>
-      <span>© 2024 jankier. All rights reserved.</span>
+      <div className="footer-section">
+        <div className="box"></div>
+        <div>© 2024 jankier. All rights reserved.</div>
+      </div>
     </div>
   );
 };
