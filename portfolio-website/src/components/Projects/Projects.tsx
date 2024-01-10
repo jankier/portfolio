323fc8d7f133077446c68db19d1dd58fb4e0c9b0
Jan Kierejsza <jan.kierejsza@gmail.com>
2024-01-10 03:21:56 +0100

    animation-addition-img-addiditon

--- portfolio-website/src/components/Projects/Projects.tsx
+++ portfolio-website/src/components/Projects/Projects.tsx
@@ -2,6 +2,7 @@ import { motion, useInView } from "framer-motion";
 import { useRef } from "react";
 import { ProjectType, SelectedPage } from "../../shared/types";
 import { projects_elements } from "../../shared/projects_elements";
+import useMediaQuery from "../../hooks/useMediaQuery";
 import "./Projects.css";
 
 type Props = {
@@ -9,8 +10,13 @@ type Props = {
 };
 
 const Projects = ({ setSelectedPage }: Props) => {
+  const isAboveMediumScreen = useMediaQuery("(min-width: 1060px)");
+
   const project_ref = useRef(null);
-  const isInView_project = useInView(project_ref, { once: true });
+  const isInView_project = useInView(project_ref, {
+    once: true,
+    amount: isAboveMediumScreen ? 1 : 0.2,
+  });
 
   return (
     <div id="projects" className="projects">
@@ -51,18 +57,24 @@ const Projects = ({ setSelectedPage }: Props) => {
               key={i}
               initial={{
                 opacity: 0,
-                y: -20,
-                scale: 0.5,
+                y: 100,
               }}
               animate={{
                 opacity: isInView_project ? 1 : 0,
-                y: isInView_project ? 0 : 20,
-                scale: isInView_project ? 1 : 0.5,
+                y: isInView_project ? 0 : 100,
               }}
-              transition={{ duration: 0.3, delay: i * 0.2 }}
+              transition={{ duration: 0.3, delay: i * 0.3 }}
             >
-              <div className="single-project-img">{project.image}</div>
-              <span>{project.title}</span>
+              <div className={`single-project-bg c${i}`}></div>
+              <div className="single-project-img">
+                {project.image}
+                <div className="project-desc">
+                  <div>
+                    <span>{project.desc}</span>
+                  </div>
+                </div>
+              </div>
+              <div className="project-title">{project.title}</div>
               <div className="project-buttons">
                 <a
                   className="project-btn"
