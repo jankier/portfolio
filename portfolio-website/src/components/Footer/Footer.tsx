84f5f3a3c4819b738da1728d99a194edd218a53d
Jan Kierejsza <jan.kierejsza@gmail.com>
2024-01-10 03:21:21 +0100

    footer-added

---
+++ portfolio-website/src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="box"></div>
+      <span>© 2024 jankier. All rights reserved.</span>
+    </div>
+  );
+};
+
+export default Footer;
